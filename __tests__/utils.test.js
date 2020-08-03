b35878f86f317a6884d0180bd0bf336c4585f549
Joshua Simpson <simpsonjosh16@gmail.com>
2020-08-03 16:58:58 +0100

    Completed formatDates function + tests

--- __tests__/utils.test.js
+++ __tests__/utils.test.js
@@ -28,8 +28,55 @@ describe('formatDates', () => {
 
     expect(formatDates(input)).toEqual(output)
   })
+  test('works for multiple objects', () => {
+    const input = [{
+      title: 'Living in the shadow of a great man',
+      topic: 'mitch',
+      author: 'butter_bridge',
+      body: 'I find this existence challenging',
+      created_at: 1542284514171,
+      votes: 100
+    }, {
+      body: "I am 100% sure that we're not completely sure.",
+      belongs_to: 'UNCOVERED: catspiracy to bring down democracy',
+      created_by: 'butter_bridge',
+      votes: 1,
+      created_at: 1069850163389
+    }, {
+      title: 'Student SUES Mitch!',
+      topic: 'mitch',
+      author: 'rogersop',
+      body:
+        'We all love Mitch and his wonderful, unique typing style. However, the volume of his typing has ALLEGEDLY burst another students eardrums, and they are now suing for damages',
+      created_at: 1163852514171,
+    }]
+
+    const output = [{
+      title: 'Living in the shadow of a great man',
+      topic: 'mitch',
+      author: 'butter_bridge',
+      body: 'I find this existence challenging',
+      created_at: '11/15/2018 @ 12:21:54 PM',
+      votes: 100
+    }, {
+      body: "I am 100% sure that we're not completely sure.",
+      belongs_to: 'UNCOVERED: catspiracy to bring down democracy',
+      created_by: 'butter_bridge',
+      votes: 1,
+      created_at: '11/26/2003 @ 12:36:03 PM'
+    }, {
+      title: 'Student SUES Mitch!',
+      topic: 'mitch',
+      author: 'rogersop',
+      body:
+        'We all love Mitch and his wonderful, unique typing style. However, the volume of his typing has ALLEGEDLY burst another students eardrums, and they are now suing for damages',
+      created_at: '11/18/2006 @ 12:21:54 PM',
+    }]
+
+    expect(formatDates(input)).toEqual(output)
+  });
 });
 
-describe('makeRefObj', () => {});
+describe('makeRefObj', () => { });
 
-describe('formatComments', () => {});
+describe('formatComments', () => { });
--- db/utils/utils.js
+++ db/utils/utils.js
@@ -1,14 +1,31 @@
 exports.formatDates = list => {
-  if(list.length === 0){
+  let i = 0
+
+  if (list.length === 0) {
     return []
   }
-  const changeToDate = new Date(list[0].created_at).toLocaleDateString("en-UK")
-  const changeToTime = new Date(list[0].created_at).toLocaleTimeString("en-UK")
-  console.log(`${changeToDate} @ ${changeToTime}`)
 
-  list[0].created_at = `${changeToDate} @ ${changeToTime}`
+  // list.forEach((object) => {
+    
+  //   const changeToDate = new Date(object.created_at).toLocaleDateString("en-UK")
+  //   const changeToTime = new Date(object.created_at).toLocaleTimeString("en-UK")
+  //   // const changeToDate = new Date(object.created_at).toDateString()
+  //   // console.log(changeToDate, changeToTime)
+  //   newList.push(object)
+  //   newList[i]['created_at'] = `${changeToDate} @ ${changeToTime}`
+  //   i += 1
+  // })
+  const newList = list.map((object) => {
+    const changeToDate = new Date(object.created_at).toLocaleDateString("en-UK")
+    const changeToTime = new Date(object.created_at).toLocaleTimeString("en-UK")
+  //   // const changeToDate = new Date(object.created_at).toDateString()
+  //   // console.log(changeToDate, changeToTime)
+  //   newList.push(object)
+     object['created_at'] = `${changeToDate} @ ${changeToTime}`
+     return object
+  })
 
-  return list
+return newList
 };
 
 exports.makeRefObj = list => { };
