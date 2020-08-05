aaf441f204bfc31aee0cb9b09668d603c7811a1b
Joshua Simpson <simpsonjosh16@gmail.com>
2020-08-05 15:54:49 +0100

    completed users/:username

--- db/utils/utils.js
+++ db/utils/utils.js
@@ -23,7 +23,6 @@ exports.makeRefObj = (table, oldKey, newValue) => {
 
 
 exports.formatComments = ({comments}, articleRef) => {
-  console.log(comments, articleRef)
 
   return comments.map(({ belongs_to, created_at, created_by, ...rest }) => {
     return { 'author': created_by, 'article_id': articleRef[belongs_to], 'created_at': new Date(created_at), ...rest };
